c4cd0774305395191a4eadad098df2c93a63ef9a
andreyyste <manurungandre1927@gmail.com>
2026-06-01 19:28:12 +0700

    fix/scroll: remove nested overflow-x-hidden to restore native smooth mouse scroll inertia on desktop

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -8,9 +8,9 @@ interface LayoutProps {
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
-        <div className="bg-background text-on-background font-body-md min-h-screen flex flex-col relative w-full overflow-x-hidden">
+        <div className="bg-background text-on-background font-body-md min-h-screen flex flex-col relative w-full">
             <Navbar />
-            <main className="flex-grow w-full mx-auto flex flex-col overflow-x-hidden">
+            <main className="flex-grow w-full mx-auto flex flex-col">
                 {children}
             </main>
             <Footer />
