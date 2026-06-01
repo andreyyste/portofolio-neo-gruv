9563e88b6ae6bbcbcee3f1e14d7ddbd92277bcbc
andreyyste <manurungandre1927@gmail.com>
2026-06-01 19:37:45 +0700

    fix/layout: add overflow-x-hidden class back to body element to contain horizontal scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body className="overflow-x-hidden">{children}</body>
     </html>
   );
 }
